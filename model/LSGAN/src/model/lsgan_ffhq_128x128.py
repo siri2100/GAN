e34d148debb5d9305a6f63dc0333b2292fb98081
jjkim95 <80367630+siri2100@users.noreply.github.com>
2022-04-28 20:22:38 +0900

    Update lsgan_ffhq_128x128.py

--- model/LSGAN/src/model/lsgan_ffhq_128x128.py
+++ model/LSGAN/src/model/lsgan_ffhq_128x128.py
@@ -2,20 +2,16 @@ import torch.nn as nn
 
 
 class Generator(nn.Module):
-    def __init__(self, dim_x, dim_z):
+    def __init__(self, dim_z):
         super(Generator, self).__init__()
-        self.dim_z = dim_z
-        self.dim_x = dim_x
-        self.fc = nn.Linear(dim_z, 1024*(4**2))
-        self.deconv1 = nn.ConvTranspose2d(1024, 512, 4, 2, 1, bias=False)
-        self.deconv2 = nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False)
-        self.deconv3 = nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False)
-        self.deconv4 = nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False)
-        self.deconv5 = nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False)
+        self.fc = nn.Linear(dim_z, 512*(4**2))
+        self.deconv1 = nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False)
+        self.deconv2 = nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False)
+        self.deconv3 = nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False)
+        self.deconv4 = nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False)
         self.bn1 = nn.BatchNorm2d(512)
         self.bn2 = nn.BatchNorm2d(256)
         self.bn3 = nn.BatchNorm2d(128)
-        self.bn4 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(True)
         self.tanh = nn.Tanh()
         self._weight_init()
@@ -26,8 +22,7 @@ class Generator(nn.Module):
         x = self.relu(self.bn1(self.deconv1(z)))
         x = self.relu(self.bn2(self.deconv2(x)))
         x = self.relu(self.bn3(self.deconv3(x)))
-        x = self.relu(self.bn4(self.deconv4(x)))
-        x = self.tanh(self.deconv5(x))  
+        x = self.tanh(self.deconv4(x))
         return x
     
     def _weight_init(self):
@@ -35,30 +30,25 @@ class Generator(nn.Module):
         nn.init.normal_(self.deconv2.weight.data, 0.0, 0.02)
         nn.init.normal_(self.deconv3.weight.data, 0.0, 0.02)
         nn.init.normal_(self.deconv4.weight.data, 0.0, 0.02)
-        nn.init.normal_(self.deconv5.weight.data, 0.0, 0.02)
         nn.init.normal_(self.bn1.weight.data, 1.0, 0.02)
         nn.init.normal_(self.bn2.weight.data, 1.0, 0.02)
         nn.init.normal_(self.bn3.weight.data, 1.0, 0.02)
-        nn.init.normal_(self.bn4.weight.data, 1.0, 0.02)
         nn.init.constant_(self.bn1.bias.data, 0.0)
         nn.init.constant_(self.bn2.bias.data, 0.0)
         nn.init.constant_(self.bn3.bias.data, 0.0)
-        nn.init.constant_(self.bn4.bias.data, 0.0)
 
 
 class Discriminator(nn.Module):
-    def __init__(self, dim_x):
+    def __init__(self):
         super(Discriminator, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, 4, 2, 1, bias=False)
         self.conv2 = nn.Conv2d(64, 128, 4, 2, 1, bias=False)
         self.conv3 = nn.Conv2d(128, 256, 4, 2, 1, bias=False)
         self.conv4 = nn.Conv2d(256, 512, 4, 2, 1, bias=False)
-        self.conv5 = nn.Conv2d(512, 1024, 4, 2, 1, bias=False)
-        self.conv6 = nn.Conv2d(1024, 1, 4, 1, 0, bias=False)
+        self.conv5 = nn.Conv2d(512, 1, 4, 1, 0, bias=False)
         self.bn2 = nn.BatchNorm2d(128)
         self.bn3 = nn.BatchNorm2d(256)
         self.bn4 = nn.BatchNorm2d(512)
-        self.bn5 = nn.BatchNorm2d(1024)
         self.lrelu = nn.LeakyReLU(0.2, inplace=True)
         self._weight_init()
         
@@ -67,8 +57,7 @@ class Discriminator(nn.Module):
         x = self.lrelu(self.bn2(self.conv2(x)))
         x = self.lrelu(self.bn3(self.conv3(x)))
         x = self.lrelu(self.bn4(self.conv4(x)))
-        x = self.lrelu(self.bn5(self.conv5(x)))
-        x = self.conv6(x)
+        x = self.conv5(x)
         return x
 
     def _weight_init(self):
@@ -77,12 +66,9 @@ class Discriminator(nn.Module):
         nn.init.normal_(self.conv3.weight.data, 0.0, 0.02)
         nn.init.normal_(self.conv4.weight.data, 0.0, 0.02)
         nn.init.normal_(self.conv5.weight.data, 0.0, 0.02)
-        nn.init.normal_(self.conv6.weight.data, 0.0, 0.02)
         nn.init.normal_(self.bn2.weight.data, 1.0, 0.02)
         nn.init.normal_(self.bn3.weight.data, 1.0, 0.02)
         nn.init.normal_(self.bn4.weight.data, 1.0, 0.02)
-        nn.init.normal_(self.bn5.weight.data, 1.0, 0.02)
         nn.init.constant_(self.bn2.bias.data, 0.0)
         nn.init.constant_(self.bn3.bias.data, 0.0)
         nn.init.constant_(self.bn4.bias.data, 0.0)
-        nn.init.constant_(self.bn5.bias.data, 0.0)
